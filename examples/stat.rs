3161e743933c317a9df6d621ac50965723c3ec2c
Mihail Konev <k.mvc@ya.ru>
2017-02-06 04:07:31 +0500

    Add maybe_cygwin_symlink()

---
+++ examples/stat.rs
@@ -0,0 +1,25 @@
+extern crate cygwin_symlinks;
+
+use std::path::Path;
+
+#[cfg(not(windows))]
+fn stat_path(path: &Path) {
+    println!("{:?}", path);
+}
+
+#[cfg(windows)]
+fn stat_path(path: &Path) {
+    println!("{:?}", path);
+
+    let maybe_cygwin_symlink = cygwin_symlinks::maybe_cygwin_symlink(&path);
+    println!("  maybe cygwin symlink: {}", maybe_cygwin_symlink);
+}
+
+fn main() {
+    let mut first_arg = true;
+    for arg in std::env::args_os() {
+        if first_arg { first_arg = false; continue; }
+        let path = Path::new(&arg);
+        stat_path(path);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+#[cfg(windows)]
+extern crate kernel32;
+#[cfg(windows)]
+extern crate winapi;
+
+#[cfg(windows)]
+use std::os::windows::ffi::OsStrExt;
+#[cfg(windows)]
+use std::iter::once;
+#[cfg(windows)]
+use std::path::{Path};
+#[cfg(windows)]
+use std::vec::Vec;
+
+#[cfg(windows)]
+use winapi::winnt::{
+    FILE_ATTRIBUTE_SYSTEM,
+};
+
+#[cfg(windows)]
+pub fn maybe_cygwin_symlink(path: &Path) -> bool {
+    let path_wz: Vec<u16> = path.as_os_str().encode_wide().chain(once(0)).collect();
+    let attr = unsafe {
+        kernel32::GetFileAttributesW(path_wz.as_ptr())
+    };
+    return (attr & FILE_ATTRIBUTE_SYSTEM) != 0;
 }
