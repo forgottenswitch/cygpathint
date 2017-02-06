13325f3faf25fe1f02e275d9f579eb0749c05215
Mihail Konev <k.mvc@ya.ru>
2017-02-06 05:51:22 +0500

    add root path discovery

--- examples/stat.rs
+++ examples/stat.rs
@@ -2,24 +2,26 @@ extern crate cygwin_path;
 
 use std::path::Path;
 
-#[cfg(not(windows))]
-fn stat_path(path: &Path) {
-    println!("{:?}", path);
-}
-
-#[cfg(windows)]
-fn stat_path(path: &Path) {
+fn stat_path(path: &Path, cygpath: &cygwin_path::CygwinPath) {
     println!("{:?}", path);
 
-    let maybe_cygwin_symlink = cygwin_path::maybe_cygwin_symlink(&path);
-    println!("  maybe cygwin symlink: {}", maybe_cygwin_symlink);
+    if !cygpath.running_under_cygwin() {
+        println!("  Not running under cygwin.");
+    } else {
+        println!("  Cygwin root: {:?}", cygpath.root_path());
+
+        let maybe_cygwin_symlink = cygwin_path::maybe_cygwin_symlink(&path);
+        println!("  Maybe cygwin symlink: {}", maybe_cygwin_symlink);
+    }
 }
 
 fn main() {
+    let cygpath = cygwin_path::CygwinPath::new();
+
     let mut first_arg = true;
     for arg in std::env::args_os() {
         if first_arg { first_arg = false; continue; }
         let path = Path::new(&arg);
-        stat_path(path);
+        stat_path(path, &cygpath);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,13 @@ extern crate kernel32;
 extern crate winapi;
 
 #[cfg(windows)]
+use std::ffi::OsString;
+#[cfg(windows)]
 use std::os::windows::ffi::OsStrExt;
 #[cfg(windows)]
 use std::iter::once;
 #[cfg(windows)]
-use std::path::{Path};
+use std::path::{Path,PathBuf};
 #[cfg(windows)]
 use std::vec::Vec;
 
@@ -16,6 +18,86 @@ use std::vec::Vec;
 use winapi::winnt::{
     FILE_ATTRIBUTE_SYSTEM,
 };
+
+#[cfg(windows)]
+fn find_in_pathlist(pathlist: &Option<OsString>, filename: &Path) -> Option<PathBuf> {
+    match pathlist {
+        &None => None,
+        &Some(ref pathlist_os) => {
+            for dir in std::env::split_paths(&pathlist_os) {
+                let filepath = dir.join(filename);
+                if filepath.is_file() {
+                    return Some(filepath);
+                }
+            }
+            None
+        }
+    }
+}
+
+#[cfg(windows)]
+pub struct CygwinPath {
+    native_path_to_root: PathBuf,
+    running_under_cygwin: bool,
+}
+
+#[cfg(not(windows))]
+pub struct CygwinPath {
+}
+
+impl CygwinPath {
+    #[cfg(not(windows))]
+    pub fn new() -> CygwinPath {
+        CygwinPath {}
+    }
+
+    #[cfg(not(windows))]
+    pub fn running_under_cygwin(&self) -> bool {
+        false
+    }
+
+    #[cfg(windows)]
+    pub fn new() -> CygwinPath {
+        let env_path = std::env::var_os("PATH");
+        let cygwin_dll_name = Path::new("cygwin1.dll");
+        let mut under_cygwin = false;
+        let root =
+            match find_in_pathlist(&env_path, &cygwin_dll_name) {
+                None => PathBuf::new(),
+                Some(cygwin_dll_path) =>
+                    match cygwin_dll_path.parent() {
+                        None => PathBuf::new(),
+                        Some(bin_path) =>
+                            match bin_path.parent() {
+                                None => PathBuf::new(),
+                                Some(root_path) => {
+                                    under_cygwin = true;
+                                    PathBuf::new().join(root_path)
+                                }
+                            },
+                    },
+            };
+        CygwinPath {
+            running_under_cygwin: under_cygwin,
+            native_path_to_root: root,
+        }
+    }
+
+    #[cfg(windows)]
+    pub fn root_path(&self) -> &Path {
+        self.native_path_to_root.as_path()
+    }
+
+    #[cfg(windows)]
+    pub fn running_under_cygwin(&self) -> bool {
+        self.running_under_cygwin
+    }
+}
+
+#[cfg(not(windows))]
+pub fn maybe_cygwin_symlink(path: &Path) -> bool {
+    false
+}
 
 #[cfg(windows)]
 pub fn maybe_cygwin_symlink(path: &Path) -> bool {
